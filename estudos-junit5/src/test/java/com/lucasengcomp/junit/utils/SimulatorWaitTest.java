3a9620ff8aff3268ed5996f6ede8291b86c0f307
Lucas Galvao Lima <lucas.engcomp@outlook.com>
2023-11-25 20:19:40 -0300

    :test_tube:	`test` Added annotation disable tests

--- estudos-junit5/src/test/java/com/lucasengcomp/junit/utils/SimulatorWaitTest.java
+++ estudos-junit5/src/test/java/com/lucasengcomp/junit/utils/SimulatorWaitTest.java
@@ -1,5 +1,6 @@
 package com.lucasengcomp.junit.utils;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import java.time.Duration;
@@ -9,11 +10,13 @@ import static org.junit.jupiter.api.Assertions.*;
 class SimulatorWaitTest {
 
     @Test
+    @Disabled("Test disabled")
     void mustWaitTimeOut() {
         assertTimeout(Duration.ofSeconds(1), ()-> SimulatorWait.wait(Duration.ofMillis(10)));
     }
 
     @Test
+    @Disabled("Test disabled")
     void doesWaitTimeOut() {
         assertTimeoutPreemptively(Duration.ofSeconds(1), ()-> SimulatorWait.wait(Duration.ofMillis(10)));
     }
