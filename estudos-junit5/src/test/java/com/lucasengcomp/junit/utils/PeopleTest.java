7174ef9a48e5eb0e43ee4099037d04a51e6d5e88
Lucas Galvao Lima <lucas.engcomp@outlook.com>
2023-11-25 20:28:13 -0300

    :test_tube:	`test` Added annotation to run test only environment = PROD

--- estudos-junit5/src/test/java/com/lucasengcomp/junit/utils/PeopleTest.java
+++ estudos-junit5/src/test/java/com/lucasengcomp/junit/utils/PeopleTest.java
@@ -1,14 +1,17 @@
 package com.lucasengcomp.junit.utils;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.EnabledIfEnvironmentVariable;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class PeopleTest {
 
-
     @Test
+    @EnabledIfEnvironmentVariable(named = "ENV", matches = "PROD")
     void groupedAssertion() {
+//        Assumptions.assumeTrue("PROD".equals(System.getenv("ENV")), () -> "Aborting test... Should not be run in a PROD environment");
         People people = new People("Lucas", "Galvao");
 
         assertAll("Assertions people",
