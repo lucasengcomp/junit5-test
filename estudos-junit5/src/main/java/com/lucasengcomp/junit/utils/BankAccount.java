c4846face8d92dcc4fa03be2ad593e64eaef384a
Lucas Galvao Lima <lucas.engcomp@outlook.com>
2023-11-26 11:48:44 -0300

    :test_tube:	`test` Added tests BankAccount

--- estudos-junit5/src/main/java/com/lucasengcomp/junit/utils/BankAccount.java
+++ estudos-junit5/src/main/java/com/lucasengcomp/junit/utils/BankAccount.java
@@ -13,6 +13,31 @@ public class BankAccount {
         this.balance = balance;
     }
 
+    public void withdraw(BigDecimal value) {
+        if (value == null || value.compareTo(BigDecimal.ONE) <= 0) {
+            throw new IllegalArgumentException("Invalid value");
+        }
+        if (this.balance.compareTo(value) <= 0) {
+            throw new RuntimeException("Insufficient funds");
+        }
+        subtractBalance(value);
+    }
+
+    public void deposit(BigDecimal valor) {
+        if (valor == null || valor.compareTo(BigDecimal.ZERO) <= 0) {
+            throw new IllegalArgumentException("The value must be greater than zero");
+        }
+        addBalance(valor);
+    }
+
+    private void addBalance(BigDecimal value) {
+        balance = balance.add(value);
+    }
+
+    private void subtractBalance(BigDecimal value) {
+        this.balance = this.balance.subtract(value);
+    }
+
     public BigDecimal balanceAccount() {
         return this.balance;
     }
--- estudos-junit5/src/test/java/com/lucasengcomp/junit/utils/BankAccountTest.java
+++ estudos-junit5/src/test/java/com/lucasengcomp/junit/utils/BankAccountTest.java
@@ -10,6 +10,39 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 class BankAccountTest {
 
     @Test
+    void shouldGetBalance() {
+        BankAccount account = new BankAccount(new BigDecimal("73.00"));
+        assertEquals(new BigDecimal("73.00"), account.balanceAccount());
+    }
+
+    @Test
+    void shouldThrowExceptionWhenTheDepositLessThanZeroAndDisplayMessage() {
+        BankAccount account = new BankAccount(new BigDecimal("73.00"));
+        IllegalArgumentException exception =
+                assertThrows(IllegalArgumentException.class, () -> account.deposit(new BigDecimal("-10.00")));
+        assertEquals("The value must be greater than zero", exception.getMessage());
+    }
+
+    @Test
+    void shouldThrowExceptionWhenTheDepositLessThanZero() {
+        BankAccount account = new BankAccount(new BigDecimal("73.00"));
+        assertThrows(IllegalArgumentException.class, () -> account.deposit(new BigDecimal("-10.00")));
+    }
+
+    @Test
+    void shouldThrowExceptionWhenTheDepositIsZero() {
+        BankAccount account = new BankAccount(new BigDecimal("73.00"));
+        assertThrows(IllegalArgumentException.class, () -> account.deposit(new BigDecimal("0.00")));
+    }
+
+    @Test
+    void shouldThrowExceptionWhenTryDepositValueZero() {
+        BankAccount account = new BankAccount(new BigDecimal("73.00"));
+        account.deposit(new BigDecimal("100.00"));
+        assertEquals(new BigDecimal("173.00"), account.balanceAccount());
+    }
+
+    @Test
     void shouldThrowExceptionWhenTheBalanceIsNull() {
         assertThrows(IllegalArgumentException.class, () -> new BankAccount(null));
     }
@@ -25,5 +58,42 @@ class BankAccountTest {
         BigDecimal testBalance = new BigDecimal("73.00");
         BankAccount account = new BankAccount(testBalance);
         assertEquals(testBalance, account.balanceAccount(), "Balance should be set correctly");
+    }
+
+    @Test
+    void shouldWithdrawFromTheAccount() {
+        BankAccount account = new BankAccount(new BigDecimal("73.00"));
+        account.withdraw(new BigDecimal("20.00"));
+        assertEquals(new BigDecimal("53.00"), account.balanceAccount());
+    }
+
+    @Test
+    void shouldThrowExceptionWhenValueAccountThanZero() {
+        BankAccount account = new BankAccount(new BigDecimal("0.00"));
+        assertThrows(RuntimeException.class, () -> account.withdraw(BigDecimal.TEN));
+    }
+
+    @Test
+    void shouldThrowExceptionWhenValueAccountLessThanZero() {
+        BankAccount account = new BankAccount(new BigDecimal("-0.01"));
+        assertThrows(RuntimeException.class, () -> account.withdraw(BigDecimal.TEN));
+    }
+
+    @Test
+    void shouldThrowExceptionWhenValueIsNull() {
+        BankAccount account = new BankAccount(new BigDecimal("73.00"));
+        assertThrows(IllegalArgumentException.class, () -> account.withdraw(null));
+    }
+
+    @Test
+    void shouldThrowExceptionWhenValueIsZero() {
+        BankAccount account = new BankAccount(new BigDecimal("73.00"));
+        assertThrows(IllegalArgumentException.class, () -> account.withdraw(BigDecimal.ZERO));
+    }
+
+    @Test
+    void shouldThrowExceptionWhenValueIsLessThenZero() {
+        BankAccount account = new BankAccount(new BigDecimal("73.00"));
+        assertThrows(IllegalArgumentException.class, () -> account.withdraw(new BigDecimal(-10)));
     }
 }
