c8c1c1187d4849593d4362d926228c644a0a4af0
Lucas Galvao Lima <lucas.engcomp@outlook.com>
2023-12-03 13:14:52 -0300

    :test_tube: `test` Added lib junit-jupiter. Added annotations mocks objects

--- estudos-junit5-blog/src/test/java/com/lucasengcomp/junit/blog/business/RegisterEditorWithMockTest.java
+++ estudos-junit5-blog/src/test/java/com/lucasengcomp/junit/blog/business/RegisterEditorWithMockTest.java
@@ -3,11 +3,13 @@ package com.lucasengcomp.junit.blog.business;
 import com.lucasengcomp.junit.blog.CustomDisplayNameGenerator;
 import com.lucasengcomp.junit.blog.model.Editor;
 import com.lucasengcomp.junit.blog.storage.StorageEditor;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.math.BigDecimal;
 
@@ -15,22 +17,28 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
+@ExtendWith(MockitoExtension.class)
 @DisplayNameGeneration(CustomDisplayNameGenerator.class)
 class RegisterEditorWithMockTest {
 
-    EditorRegistration editorRegistration;
     Editor editor;
+
+    @Mock
+    StorageEditor storageEditor;
+
+    @Mock
+    EmailSendingManager emailSendingManager;
+
+    @InjectMocks
+    EditorRegistration editorRegistration;
+
 
     @BeforeEach
     void setUp() {
         editor = new Editor(null, "Lucas", "lucas@email.com", BigDecimal.TEN, true);
 
-        StorageEditor storageEditor = mock(StorageEditor.class);
         when(storageEditor.save(editor))
                 .thenReturn(new Editor(1L, "Lucas", "lucas@email.com", BigDecimal.TEN, true));
-        EmailSendingManager emailSendingManager = mock(EmailSendingManager.class);
-
-        editorRegistration = new EditorRegistration(storageEditor, emailSendingManager);
     }
 
     @Test
