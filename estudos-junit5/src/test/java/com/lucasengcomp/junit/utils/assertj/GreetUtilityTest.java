6195c45766adc260dcdd2ffdc0d3cc895b8eada3
Lucas Galvao Lima <lucas.engcomp@outlook.com>
2023-12-10 13:10:32 -0300

    :test_tube: `test` Added test throw exception with AssertJ

--- estudos-junit5/src/test/java/com/lucasengcomp/junit/utils/assertj/GreetUtilityTest.java
+++ estudos-junit5/src/test/java/com/lucasengcomp/junit/utils/assertj/GreetUtilityTest.java
@@ -6,8 +6,9 @@ import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.Test;
 
 import static com.lucasengcomp.junit.utils.GreetUtility.greet;
-import static com.lucasengcomp.junit.utils.MessagesUtil.*;
-import static org.junit.jupiter.api.Assertions.*;
+import static com.lucasengcomp.junit.utils.MessagesUtil.GOOD_M0RNING;
+import static com.lucasengcomp.junit.utils.MessagesUtil.INVALID_HOUR;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @DisplayNameGeneration(CustomDisplayNameGenerator.class)
 class GreetUtilityTest {
@@ -20,5 +21,12 @@ class GreetUtilityTest {
         Assertions.assertThat(greetGoodMorning)
                 .withFailMessage(INVALID_HOUR)
                 .isEqualTo(GOOD_M0RNING);
+    }
+
+    @Test
+    void shouldThrowException() {
+        Assertions.assertThatThrownBy(() -> greet(-10))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(INVALID_HOUR);
     }
 }
